1d9ce8dbab6d67ae6c40183df6d5d11ee8508630
johnseth97 <17620345+johnseth97@users.noreply.github.com>
2025-07-13 17:49:01 -0400

    fixed port bindings to avoid conflicts in dev

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import { Request, Response } from 'express'
 import photosRouter from './routes/photos'
 
 const app = express()
@@ -7,6 +8,11 @@ const PORT = process.env.PORT || 5000 // Defualt port, can be overridden by envi
 app.use(express.json())
 
 app.use('/api/photos', photosRouter)
+
+// Health check endpoint
+app.use('/api/health', (_req: Request, res: Response) => {
+  res.status(200).json({ status: 'OK' })
+})
 
 app.listen(PORT, () => {
   console.log(`Backend running on port ${PORT}`)
